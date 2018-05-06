de2cdffe59e3ba7c99157bf7c5b617d16bb60602
jeremiehvt <jeremiehvt@gmail.com>
2018-05-06 20:07:59 +0200

    add new js file

---
+++ public/js/modifyDatepicker.js
@@ -0,0 +1,34 @@
+
+$(function(){
+ 	
+ 	var year = new Date(); 
+ 	
+	
+	$.fn.datepicker.dates['fr'] = {
+		days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],
+		daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],
+		daysMin: ["d", "l", "ma", "me", "j", "v", "s"],
+		months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
+		monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
+		today: "Aujourd'hui",
+		monthsTitle: "Mois",
+		clear: "Effacer",
+		weekStart: 1,
+		format: "dd/mm/yyyy"
+	};
+
+	console.log((year.getFullYear()+2)+"-12-31");
+
+	$( "#command_visitDay" ).datepicker({
+	  startDate: new Date(),
+	  endDate: (year.getFullYear()+2)+"-12-31",
+	  language: 'fr',
+	  format: 'yyyy-mm-dd',
+	  daysOfWeekDisabled: '2',
+	  datesDisabled: [ ""+ year.getFullYear()+"-05-01", ""+ year.getFullYear()+"-11-01", ""+ year.getFullYear()+"-12-25",
+	  ""+ (year.getFullYear()+1)+"-05-01", ""+ (year.getFullYear()+1)+"-11-01", ""+ (year.getFullYear()+1)+"-12-25",
+	  ""+ (year.getFullYear()+2)+"-05-01", ""+ (year.getFullYear()+2)+"-11-01", ""+ (year.getFullYear()+2)+"-12-25"]
+	});
+
+	$( "#command_visitDay" ).attr('placeholder', 'année-mois-jour');
+});
